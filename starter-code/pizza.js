515872e9d7cd7cefda9b81a53ef2a1f5f15f14e7
Jad ch <jad.chalita@gmail.com>
2017-10-16 22:58:39 +0200

    Done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -30,6 +30,21 @@ const vm = new Vue({
     isSelected(ingredientName) {
       // Given the name of an ingredient, will return whether it is selected
       return this.ingredients.find(ingredient => ingredient.name === ingredientName).selected
+    }   
+  },
+  filters: {
+    capitalize: function (ingredient) {
+      if (!ingredient.name) return 's'
+      name = ingredient.name.toString()
+      return name.charAt(0).toUpperCase() + name.slice(1)
+    }
+  },
+  computed: {
+    // Total Price
+    totalPrice: function () {
+      return  this.basePrice + this.ingredients.reduce( (total, ingredient) => {
+        return ingredient.selected ? total + ingredient.price : total
+      }, 0)
     }
   }
 });
