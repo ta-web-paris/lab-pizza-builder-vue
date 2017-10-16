dc313710946c6a950fe6dc82fc9893b7eb264940
ombline <ombline90@hotmail.com>
2017-10-16 20:47:41 +0200

    finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,23 +4,27 @@ const vm = new Vue({
     basePrice: 10,
     ingredients: [
       {
-        name: 'pepperonni',
+        name: "pepperonni",
         price: 1,
         selected: false
-      }, {
-        name: 'mushrooms',
+      },
+      {
+        name: "mushrooms",
         price: 1,
         selected: true
-      }, {
-        name: 'green peppers',
+      },
+      {
+        name: "green peppers",
         price: 1,
         selected: true
-      }, {
-        name: 'white sauce',
+      },
+      {
+        name: "white sauce",
         price: 3,
         selected: false
-      }, {
-        name: 'gluten-free crust',
+      },
+      {
+        name: "gluten-free crust",
         price: 5,
         selected: false
       }
@@ -29,7 +33,30 @@ const vm = new Vue({
   methods: {
     isSelected(ingredientName) {
       // Given the name of an ingredient, will return whether it is selected
-      return this.ingredients.find(ingredient => ingredient.name === ingredientName).selected
+      return this.ingredients.find(
+        ingredient => ingredient.name === ingredientName
+      ).selected;
+    }
+  },
+  computed: {
+    activeIngredients: function() {
+      return this.ingredients.filter(ingredient => ingredient.selected);
+    },
+    sumIngredients: function() {
+      let count = this.basePrice;
+      this.ingredients.forEach(ingredient => {
+        if (ingredient.selected) {
+          count += ingredient.price;
+        }
+      });
+      return count;
+    }
+  },
+  filters: {
+    capitalize: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
     }
   }
 });
