fb3d885fb1c7bbda136e0d01be9e5b5b7ec5835b
Jean-Nicolas Piketty <jnpiketty@BDLCM.local>
2018-02-19 18:40:57 +0100

    PAR-WD-Pizza Vue-JN

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,26 +1,38 @@
 const vm = new Vue({
   el: "#app",
+  filters: {
+    capitalize: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+  },
   data: {
     basePrice: 10,
+
     ingredients: [
       {
-        name: 'pepperonni',
+        name: "pepperonni",
         price: 1,
         selected: false
-      }, {
-        name: 'mushrooms',
+      },
+      {
+        name: "mushrooms",
         price: 1,
         selected: true
-      }, {
-        name: 'green peppers',
+      },
+      {
+        name: "green peppers",
         price: 1,
         selected: true
-      }, {
-        name: 'white sauce',
+      },
+      {
+        name: "white sauce",
         price: 3,
         selected: false
-      }, {
-        name: 'gluten-free crust',
+      },
+      {
+        name: "gluten-free crust",
         price: 5,
         selected: false
       }
@@ -29,7 +41,15 @@ const vm = new Vue({
   methods: {
     isSelected(ingredientName) {
       // Given the name of an ingredient, will return whether it is selected
-      return this.ingredients.find(ingredient => ingredient.name === ingredientName).selected
+      return this.ingredients.find(ingredient => ingredient.name === ingredientName).selected;
+    }
+  },
+  computed: {
+    selectedIngredients: function() {
+      return this.ingredients.filter(ingredient => ingredient.selected === true);
+    },
+    totalPrice: function() {
+      return this.selectedIngredients.reduce((a, b) => a + b.price, this.basePrice);
     }
   }
 });
